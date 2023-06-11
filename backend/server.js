d9b390461a27a964d5732d636643b8ee2029a83f
Stargazing-11 <samuelkifle199314@gmail.com>
2023-06-11 20:30:39 +0300

    change on server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const book = require("./routes/bookRoute");
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect("mongodb://localhost/book_store")
   .then(() => console.log("Connected to mongodb..."))
@@ -9,5 +10,7 @@ mongoose
 const app = express();
 
 // app use
-app.use(express.json())
+app.use(express.json());
+app.use("api/books", book);
 
+app.listen(PORT, console.log(`started listening on port ${PORT}`));
